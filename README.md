bab2f3469878aa2a936a8bd971c7da729f68889b
linyiju <lynn8301@gmail.com>
2019-03-05 21:26:15 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Scrapy Project
+
+### Overview
+This Scrapy project show how to scrape the website(http://quotes.toscrape.com/).
+
+### Requirements
+* Python : 3.6.5
+* Scrapy : 1.5.1
+* System : Mac OSX 10.14.3
+
+
+# Install
+* Clone repository or down zip file directly:
+    $ git clone url(git's url)
+* Install virtual virtualenv
+	$ pip3 install virtualenv
+* Activate virtual env
+	$ cd.... (got to the root folder)
+	$ python3 -m venv vnev
+* Install requirements.txt on virtual environment.
+	$  pip3 install -r requirements.txt
+
+# Getting Start
+Now, you can use scrapy to scrape data.
+	$ scrapy crawl mybot
+If you want to store the result into the format for csv, just follow below:
+	$ scrapy crawl mybot -o `your_file name`
+These formats are supported out of the box: 
+* JSON
+* JSON lines
+* CSV
+* XML
+
+# Commercial Support
+Link:http://doc.scrapy.org/en/latest/index.html
+
---
+++ myproject/myproject/spiders/mybot.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class MybotSpider(scrapy.Spider):
+    name = 'mybot'
+    allowed_domains = ['quotes.toscrape.com']
+    start_urls = ['http://quotes.toscrape.com/']
+
+    def parse(self, response):
+
+        quotes = response.xpath('//*[@class="quote"]')
+        for quote in quotes:
+        	text = quote.xpath('.//*[@class ="text"]/text()').extract_first()
+        	author = quote.xpath('.//*[@class ="author"]/text()').extract_first()
+        	tags = quote.xpath('.//*[@class ="keywords"]/@content').extract_first()
+
+
+        	yield  {'Text' : text,
+                     'Author' :author,
+        	         'Tags':tags}
+
+       	# 翻頁
+        next_page_url = response.xpath('//*[@class="next"]/a/@href').extract_first()
+        absolute_next_page_url  = response.urljoin(next_page_url)
+        yield scrapy.Request(absolute_next_page_url)
